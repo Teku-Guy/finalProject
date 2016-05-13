3fa0589b77930fdf482b44b8780543a28407a663
Jesussalk@gmail.com <jessusalk@gmail.com>
2016-05-13 12:07:11 -0700

    Implemented Mouse Control

---
+++ 2D Shooter/src/com/input/MouseInput.java
@@ -0,0 +1,39 @@
+package com.input;
+
+/**
+ * Created by APCS1 on 5/13/2016.
+ */
+import com.entity.GameObject;
+import com.entity.ID;
+import com.entity.Player;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseInput implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
